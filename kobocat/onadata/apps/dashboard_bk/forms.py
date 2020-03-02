fab48b0a85c71d08187bfd73fc08a8316e35fd07
S M Shahinul Islam <shahinul@mpower-social.com>
2020-03-02 19:48:22 +0600

    remove unnecessary backup directories

--- kobocat/onadata/apps/dashboard_bk/forms.py
+++
@@ -1,9 +0,0 @@
-from onadata.apps.dashboard.models import *
-from django import forms
-
-
-class DashboardLoaderUpdateForm(forms.ModelForm):
-    class Meta:
-        model = DashboardLoader
-        exclude = ()
-
--- kobocat/onadata/apps/dashboard_tool_bk/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-#from __future__ import unicode_literals
-
